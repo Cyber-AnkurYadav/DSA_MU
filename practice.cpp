8d63993fa22724ea0628227572338e896dc85189
Ankur Yadav <ankuryadav9317@gmail.com>
2026-09-16 10:23:14 +0530

    array added

--- practice.cpp
+++ practice.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 int main(){
-    int a = 5;
-    cout << a << endl;
-    ++a;
-    cout << a << endl;
-    a++;
-    cout << a << endl;
+    // int a = 5;
+    // cout << a << endl;
+    // ++a;
+    // cout << a << endl;
+    // a++;
+    // cout << a << endl;
 
+    int arr[5]={2, 4, 6, 8, 9};
+
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<endl;
+    }
+    cout<<endl;
+    for(auto& i : arr){
+        cout<<i<<endl;
+    }
     return 0;
 }
